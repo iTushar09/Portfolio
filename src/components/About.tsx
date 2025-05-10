bbc8eca26974da4403d405bb8d4165400edb92a3
Tushar Chaudhari <tusharchaudhari1809@gmail.com>
2025-05-11 00:51:38 +0530

    Your commit message here

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -74,7 +74,7 @@ const About: React.FC = () => {
                   <div className="relative w-full h-full max-w-[34rem] max-h-[34rem] flex items-center justify-center">
                     <div className="absolute w-[65%] bg-black/50 backdrop-blur-sm px-4 md:px-6 py-6 md:py-8 rounded-xl text-center flex flex-col items-center">
                       <img
-                        src="/mm.png"
+                        src="/image.png"
                         className="w-26 h-26 md:w-42 md:h-42 object-cover border-2 border-teal-400 shadow-lg rounded-lg mb-4"
                       />
                     </div>
@@ -88,6 +88,7 @@ const About: React.FC = () => {
           </div>
         </div>
       </div>
+      
     </section>
   );
 };
